87f9c2324a227d4d5e4226a677e0dfaf914874ed
MatanBT <mtnbt123@gmail.com>
2021-11-18 20:35:17 +0200

    Updated tester to support shir

--- ex2/tester.py
+++ ex2/tester.py
@@ -30,14 +30,14 @@ def run_parser_on_input_files():
 
 def simple_tester():
     # for the tester to work - both directories should contains files with identical names (!).
-    print("=============== SIMPLE_TESTER ================")
+    print("\n\n=============== SIMPLE_TESTER ================")
 
     dcmp = filecmp.dircmp(output_dir, expected_output_dir)
 
     if dcmp.left_only:
         print("WARNING: The following outputs have no expected-output analogue and therefore are not tested: ", dcmp.left_only)
 
-    assert len(dcmp.diff_files) == 0, f"ERROR: The following outputs are wrong: {dcmp.diff_files}"
+    assert len(dcmp.diff_files) == 0, f"ERROR: The following outputs are wrong: " + dcmp.diff_files
     
     print("SUCCESS: Tested the following files: ", dcmp.same_files)
 
