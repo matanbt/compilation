ca515d69e265a279f2dd2465e07a4691108db5db
ShirFrenkel <65973610+ShirFrenkel@users.noreply.github.com>
2021-11-18 16:36:56 +0200

    Deleted cfield specifics

--- ex2/src/AST/AST_CFIELD.java
+++ ex2/src/AST/AST_CFIELD.java
@@ -1,12 +1,58 @@
 package AST;
 
-public abstract class AST_CFIELD extends AST_Node
+public class AST_CFIELD extends AST_Node
 {
+	/********************************/
+	/*  CField ::= funcDec | varDec */
+	/********************************/
+	public AST_DEC dec;
+
+	/*******************/
+	/*  CONSTRUCTOR(S) */
+	/*******************/
+	public AST_CFIELD(AST_DEC dec)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.print("====================== cField -> Dec\n");
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.dec = dec;
+	}
+
 	/*********************************************************/
-	/* The default message for an unknown AST cField node */
+	/* The printing message for an CFIELD FUNC AST node */
 	/*********************************************************/
 	public void PrintMe()
 	{
-		System.out.print("UNKNOWN AST CFIELD NODE");
+		/********************************************/
+		/* AST NODE TYPE = AST CFIELD FUNC */
+		/********************************************/
+		System.out.print("AST NODE CFIELD\n");
+
+		/***********************************/
+		/* RECURSIVELY PRINT FUNC */
+		/***********************************/
+		if (dec != null) dec.PrintMe();
+
+		/***************************************/
+		/* PRINT Node to AST GRAPHVIZ DOT file */
+		/***************************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			"CFIELD");
+
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+		AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,dec.SerialNumber);
 	}
 }
