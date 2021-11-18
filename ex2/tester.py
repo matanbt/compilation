90eac50fb15fdeae4ac0dec40e3556c99fcbf752
ShirFrenkel <65973610+ShirFrenkel@users.noreply.github.com>
2021-11-18 20:38:38 +0200

    fix test.py

--- ex2/tester.py
+++ ex2/tester.py
@@ -37,7 +37,7 @@ def simple_tester():
     if dcmp.left_only:
         print("WARNING: The following outputs have no expected-output analogue and therefore are not tested: ", dcmp.left_only)
 
-    assert len(dcmp.diff_files) == 0, f"ERROR: The following outputs are wrong: " + dcmp.diff_files
+    assert len(dcmp.diff_files) == 0, "ERROR: The following outputs are wrong: " + str(dcmp.diff_files)
     
     print("SUCCESS: Tested the following files: ", dcmp.same_files)
 
