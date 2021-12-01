31ed771d5c44f2bd43c738e4c05ea81adaef7f28
MatanBT <mtnbt123@gmail.com>
2021-12-01 18:48:36 +0200

    Added return SemantMe
    - Assumes TYPE_NIL is implemented

--- ex3/src/AST/AST_STMT_RETURN.java
+++ ex3/src/AST/AST_STMT_RETURN.java
@@ -1,8 +1,12 @@
 package AST;
 
+import SYMBOL_TABLE.SYMBOL_TABLE;
+import TYPES.*;
+
 public class AST_STMT_RETURN extends AST_STMT
 {
-	public AST_EXP exp; // could be null (e.g. void functions)
+	public AST_EXP exp;         // could be null (IFF void functions)
+	public AST_DEC_FUNC inFunc; // the function in which the return is in context
 
 	public AST_STMT_RETURN(AST_EXP exp)
 	{
@@ -13,6 +17,7 @@ public class AST_STMT_RETURN extends AST_STMT
 		System.out.print("SEMICOLON\n");
 
 		this.exp = exp;
+		this.inFunc = null; // will be defined in the semantic-analysis
 	}
 
 	// overloading contractors:
@@ -32,5 +37,38 @@ public class AST_STMT_RETURN extends AST_STMT
 			"RETURN");
 
 		if (exp != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, exp.SerialNumber);
+	}
+
+	/* verifies the type we return is the same as in the function declaration */
+	public TYPE SemantMe() {
+		// gets the current scope-function
+		this.inFunc = SYMBOL_TABLE.getInstance().findScopeFunc();
+
+		TYPE expectedReturnType = inFunc.result_SemantMe.rtnType;
+		TYPE actualReturnType = exp != null ? exp.SemantMe(): TYPE_VOID.getInstance();
+
+		if(actualReturnType == TYPE_NIL.getInstance()) {
+			// "return null" - is not allowed for int, string, void
+			if(expectedReturnType == TYPE_INT.getInstance()
+					|| expectedReturnType == TYPE_STRING.getInstance()
+					|| expectedReturnType == TYPE_VOID.getInstance()) {
+
+				System.out.format(">> ERROR - return null is not allowed for primitive types");
+				// TODO deal with error
+				System.exit(0);
+			}
+		}
+
+		if(expectedReturnType != actualReturnType){
+			// TODO make sure that we have a type factory, so this equal will work (i.e. I assume each type is an instance!)
+
+			System.out.format(">> ERROR actual return type (%s) is different than expected (%s)" +
+					", in function %s()", expectedReturnType.name, actualReturnType.name, inFunc.funcName);
+			// TODO deal with error
+			System.exit(0);
+		}
+
+		return null; // no TYPE for return statement
+
 	}
 }
