ec1e7d1cbc5e4e8aa248e7c86631be00a9ccf168
MatanBT <mtnbt123@gmail.com>
2021-12-03 17:41:31 +0200

    Added a new TYPE for instances of classes - DOCUMENTATION IN CODE

--- ex3/src/TYPES/TYPE_CLASS.java
+++ ex3/src/TYPES/TYPE_CLASS.java
@@ -13,18 +13,51 @@ public class TYPE_CLASS extends TYPE
 	/* packed together with the class methods         */
 	/**************************************************/
 	public TYPE_LIST data_members;
+
+	// the type this L-class defines
+	public TYPE_CLASS_INSTANCE typeOfInstancesOfThisClass;
 	
 	/****************/
 	/* CTROR(S) ... */
 	/****************/
-	public TYPE_CLASS(TYPE_CLASS father,String name,TYPE_LIST data_members)
+	public TYPE_CLASS(TYPE_CLASS father, String name, TYPE_LIST data_members)
 	{
 		this.name = name;
 		this.father = father;
 		this.data_members = data_members;
+		this.typeOfInstancesOfThisClass = new TYPE_CLASS_INSTANCE(name, this);
 	}
 
 	public boolean isSubClassOf(TYPE_CLASS potentialFather) {
 		return false; // TODO on class task: IMPLEMENT
+	}
+
+	// looks for symbol 'name' up the inheritance chain, returns null if not found
+	public TYPE_CFIELD find(String name) {
+		// look current data members
+		// didn't find ---> father.find(name)
+		return null;
+	}
+
+
+	public TYPE_CLASS_INSTANCE getInstance() {
+		return this.typeOfInstancesOfThisClass;
+	}
+
+	// we forbid assigning to class 'A := nil' when A is a class
+	public boolean canBeAssigned() {
+		return false;
+	}
+
+	public boolean canBeAssignedNil() {
+		return false;
+	}
+
+	public boolean canBeRtnType() {
+		return false; // maybe counter-intuitive, but rtnType should be 'this.typeOfInstancesOfThisClass'
+	}
+
+	public boolean isClass() {
+		return true;
 	}
 }
