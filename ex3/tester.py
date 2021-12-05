d65c1833387fe3115d0c39e7641f08fc476ff67a
MatanBT <mtnbt123@gmail.com>
2021-12-06 00:27:04 +0200

    Modified tester.py to fit the generated-tests

--- ex3/tester.py
+++ ex3/tester.py
@@ -1,29 +1,23 @@
 from subprocess import run
+import os
 
-input_dir = "input/"
-output_dir = "output/"
-expected_output_dir = "expected_output/"
+input_dir = "./input/"
+output_dir = "./output/"
+expected_output_dir = "./expected_output/"
 
 test_files = [
-    "TEST_01_Print_Primes",
-    "TEST_02_Bubble_Sort",
-    "TEST_03_Merge_Lists",
-    "TEST_04_Matrices",
-    "TEST_05_Classes",
-    "TEST_06_Print_Primes_Error",
-    "TEST_07_Bubble_Sort_Error",
-    "TEST_08_Merge_Lists_Error",
-    "TEST_09_Matrices_Error",
-    "TEST_10_Classes_Error",
+    "TEST_01_Print_Primes.txt"
      ]
 
+def list_test_files_by_dir():
+    global test_files
+    test_files = os.listdir(input_dir)
 
-
-def run_on_input_files():
+def run_on_test_files():
     # assumes all files in 'input_files' located in path 'input/'
     for filename in test_files:
-        input_file =  input_dir + filename + ".txt"          # input/test.txt
-        output_file = output_dir + filename + "_Output.txt"  # output/test_Output.txt
+        input_file =  input_dir + filename                        # input/test.txt
+        output_file = output_dir + filename[:-4] + "_Output.txt"  # output/test_Output.txt
         run(['java', '-jar', 'COMPILER', input_file, output_file])
 
 
@@ -37,12 +31,13 @@ def simple_tester():
         with open(output_file, 'r') as f1, open(expected_file, 'r') as f2:
             s1 = f1.read().strip()
             s2 = f2.read().strip()
-            assert s1 == s2, "Test failed on: " + filename
+            assert s1 == s2, "Test failed on: " + filename + "\nGot: " + s1 + "   Expected: " + s2
 
     print("\n============= SUCCESS - SIMPLE_TESTER ==============")
 
 
 
 if __name__ == '__main__':
-    run_on_input_files()
+    list_test_files_by_dir()
+    run_on_test_files()
     simple_tester()
