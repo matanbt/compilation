56e52f3337368e4ed0e1a05501f192589992a50c
ShirFrenkel <65973610+ShirFrenkel@users.noreply.github.com>
2021-12-05 20:51:09 +0200

    CR cosmetic changes

--- ex3/src/TYPES/TYPE_CLASS.java
+++ ex3/src/TYPES/TYPE_CLASS.java
@@ -24,21 +24,21 @@ public class TYPE_CLASS extends TYPE
 		this.cfield_list = cfield_list;
 	}
 
-	public TYPE findInClassAndSuperClasses(String name){
-		/* search for name in this class and it's super classes and return it's TYPE */
+	public TYPE findInClassAndSuperClasses(String cfield_name){
+		/* search for cfield_name in this class and it's super classes and return it's TYPE */
 		TYPE_CLASS curr_class = this;
 		while (curr_class != null){
 			CFIELD_LIST curr_cfield_list = curr_class.cfield_list;
 			for(CFIELD_LIST node = curr_cfield_list; node != null; node = node.next){
-				CFIELD cfild = node.head;
-				if (cfild.name.equals(name)){
-					return cfild.type;
+				CFIELD cfield = node.head;
+				if (cfield.name.equals(cfield_name)){
+					return cfield.type;
 				}
 			}
 			// name is not in caller_class, search in it's super class
 			curr_class = curr_class.father;
 		}
-		// didn't find name
+		// didn't find cfield_name
 		return null;
 	}
 }
