0378d8634ab395d6854568bc67e99b139f9b942f
ShirFrenkel <65973610+ShirFrenkel@users.noreply.github.com>
2022-01-27 12:17:02 +0200

    Fixed MIPSGen to work with getRegisterName()

--- ex4/src/MIPS/MIPSGenerator.java
+++ ex4/src/MIPS/MIPSGenerator.java
@@ -49,13 +49,13 @@ public class MIPSGenerator {
     public void allocate(String var_name) {
         fileWriter.format(".data\n");
         fileWriter.format("\t%s: .word 0\n", var_name);
-        fileWriter.format(".text\n");
+        fileWriter.format("\n.text\n");
     }
 
     public void allocateString(String var_name, String val) {
         fileWriter.format(".data\n");
         fileWriter.format("\t%s: .asciiz \"%s\"\n", var_name, val);
-        fileWriter.format(".text\n");
+        fileWriter.format("\n.text\n");
     }
 
     public void allocateWithIntVal(String name, int val) {
@@ -63,7 +63,7 @@ public class MIPSGenerator {
         // assumes name is unique
         fileWriter.format(".data\n");
         fileWriter.format("\t%s: .word %d\n", name, val);
-        fileWriter.format(".text\n");
+        fileWriter.format("\n.text\n");
     }
 
     public void allocateByReferenceName(String name, String referenceName) {
@@ -71,7 +71,7 @@ public class MIPSGenerator {
         // assumes name is unique
         fileWriter.format(".data\n");
         fileWriter.format("\t%s: .word %s\n", name, referenceName);
-        fileWriter.format(".text\n");
+        fileWriter.format("\n.text\n");
     }
 
     public void allocateWordsArray(String name, List<String> referenceNamesList) {
@@ -83,98 +83,78 @@ public class MIPSGenerator {
         for (int i = 0; i < referenceNamesList.size(); i++) {
             fileWriter.format("\t\t.word %s\n", referenceNamesList.get(i));
         }
-        fileWriter.format(".text\n");
+        fileWriter.format("\n.text\n");
     }
 
     public void malloc(TEMP dst, TEMP len) {
         /* dst = malloc(len)
         allocates 'len' amount of bytes, allocation address stored in 'dst' */
         fileWriter.format("\tli $v0, 9\n");
-        fileWriter.format("\tmove $a0, Temp_%d\n", len.getSerialNumber());
+        fileWriter.format("\tmove $a0, %s\n", len.getRegisterName());
         fileWriter.format("\tsyscall\n");
-        fileWriter.format("\tmove Temp_%d, $v0\n", dst.getSerialNumber());
+        fileWriter.format("\tmove %s, $v0\n", dst.getRegisterName());
     }
 
   public void mallocWords(TEMP dst, TEMP len) {
         // dst = malloc(len * WORD_SIZE)
         fileWriter.format("\tli $v0, 9\n");
-        fileWriter.format("\tmove $a0, Temp_%d\n", len.getSerialNumber());
+        fileWriter.format("\tmove $a0, %s\n", len.getRegisterName());
         fileWriter.format("\tmul $a0, $a0, %d\n", WORD_SIZE);
         fileWriter.format("\tsyscall\n");
-        fileWriter.format("\tmove Temp_%d, $v0\n", dst.getSerialNumber());
+        fileWriter.format("\tmove %s, $v0\n", dst.getRegisterName());
     }
 
     /* ---------------------- Load & Stores ---------------------- */
 
-    /* Loads variable from .data segment by given name */
-    public void loadGlobal(TEMP dst, String var_name) {
-        String full_var_name = String.format("global_%s", var_name);
-        this.loadByVarName(dst, full_var_name);
-    }
-
-    public void storeGlobal(String var_name, TEMP src) {
-        String full_var_name = String.format("global_%s", var_name);
-        this.storeByVarName(full_var_name, src);
-    }
-
     public void storeByVarName(String var_name, TEMP src) {
-        int idxsrc = src.getSerialNumber();
-        fileWriter.format("\tsw Temp_%d,global_%s\n", idxsrc, var_name);
+        fileWriter.format("\tsw %s, global_%s\n", src.getRegisterName(), var_name);
     }
 
     public void loadByVarName(TEMP dst, String full_var_name) {
-        int idxdst = dst.getSerialNumber();
-        fileWriter.format("\tlw Temp_%d,%s\n", idxdst, full_var_name);
+        fileWriter.format("\tlw %s, %s\n", dst.getRegisterName(), full_var_name);
     }
 
     /* Loads variable from the stack by given offset (relative to $fp) */
     public void loadFromStack(TEMP dst, int offset_in_bytes) {
-        int idxdst = dst.getSerialNumber();
-        fileWriter.format("\tlw Temp_%d,%d($fp)\n", idxdst, offset_in_bytes);
+        fileWriter.format("\tlw %s, %d($fp)\n", dst.getRegisterName(), offset_in_bytes);
     }
 
     /* Stores variable in the stack by given offset (relative to $fp) */
     public void storeToStack(int offset_in_bytes, TEMP src) {
-        int idxsrc = src.getSerialNumber();
-        fileWriter.format("\tsw Temp_%d,%d($fp)\n", idxsrc, offset_in_bytes);
+        fileWriter.format("\tsw %s, %d($fp)\n", src.getRegisterName(), offset_in_bytes);
     }
 
     public void loadFromHeap(TEMP dst, TEMP base_address, int offset_in_bytes) {
-        int idxdst = dst.getSerialNumber();
-        int idxBaseAddress = base_address.getSerialNumber();
-        fileWriter.format("\tlw Temp_%d,%d(Temp_%d)\n", idxdst, offset_in_bytes, idxBaseAddress);
+        fileWriter.format("\tlw %s, %d(%s)\n", dst.getRegisterName(), offset_in_bytes, base_address.getRegisterName());
     }
 
     public void loadFromHeap(TEMP dst, TEMP base_address, TEMP offset_in_bytes) {
-        fileWriter.format("\tadd $s0, Temp_%d, Temp_%d\n", base_address.getSerialNumber(), offset_in_bytes.getSerialNumber());
-        fileWriter.format("\tlw Temp_%d, 0($s0)\n", dst.getSerialNumber());
+        fileWriter.format("\tadd $s0, %s, %s\n", base_address.getRegisterName(), offset_in_bytes.getRegisterName());
+        fileWriter.format("\tlw %s, 0($s0)\n", dst.getRegisterName());
     }
 
     public void loadByteFromHeap(TEMP dst, TEMP base_address, int offset_in_bytes) {
-        int idxdst = dst.getSerialNumber();
-        fileWriter.format("\tlb Temp_%d, %d(Temp_%d)\n", idxdst, offset_in_bytes, base_address.getSerialNumber());
+        fileWriter.format("\tlb %s, %d(%s)\n", dst.getRegisterName(), offset_in_bytes, base_address.getRegisterName());
     }
 
     public void storeToHeap(TEMP src, TEMP base_address, int offset_in_bytes) {
-        int idxsrc = src.getSerialNumber();
-        fileWriter.format("\tsw Temp_%d, %d(Temp_%d)\n", idxsrc, offset_in_bytes, base_address.getSerialNumber());
+        fileWriter.format("\tsw %s, %d(%s)\n", src.getRegisterName(), offset_in_bytes, base_address.getRegisterName());
     }
 
     public void storeByteToHeap(TEMP src, TEMP base_address, int offset_in_bytes) {
-        int idxsrc = src.getSerialNumber();
-        int idxBaseAddress = base_address.getSerialNumber();
-        fileWriter.format("\tsw Temp_%d,%d(Temp_%d)\n", idxsrc, offset_in_bytes, idxBaseAddress);
-        fileWriter.format("\tsb Temp_%d, %d(Temp_%d)\n", idxsrc, offset_in_bytes, base_address.getSerialNumber());
+        String src_reg_name = src.getRegisterName();
+        String BaseAddress_reg_name = base_address.getRegisterName();
+        fileWriter.format("\tsw %s, %d(%s)\n", src_reg_name, offset_in_bytes, BaseAddress_reg_name);
+        fileWriter.format("\tsb %s, %d(%s)\n", src_reg_name, offset_in_bytes, base_address.getRegisterName());
     }
 
     public void storeToHeap(TEMP src, TEMP base_address, TEMP offset_in_bytes) {
-        fileWriter.format("\tadd $s0, Temp_%d, Temp_%d\n", base_address.getSerialNumber(), offset_in_bytes.getSerialNumber());
-        fileWriter.format("\tsw Temp_%d, 0($s0)\n", src.getSerialNumber());
+        fileWriter.format("\tadd $s0, %s, %s\n", base_address.getRegisterName(), offset_in_bytes.getRegisterName());
+        fileWriter.format("\tsw %s, 0($s0)\n", src.getRegisterName());
     }
 
     public void loadAddressByName(TEMP dst, String name) {
-        int idxdst = dst.getSerialNumber();
-        fileWriter.format("\tla Temp_%d, %s\n", idxdst, name);
+        fileWriter.format("\tla %s, %s\n", dst.getRegisterName(), name);
     }
   
   /* -------------- Stack access functions -------------- */
@@ -185,12 +165,12 @@ public class MIPSGenerator {
     /* Pushing a register (e.g. "$v0", "$fp", "$t5"...) to the "top" of the stack */
     public void pushRegisterToStack(String register) {
         fileWriter.format("\tsubu $sp, $sp, 4\n");
-        fileWriter.format("\tsw %s,0($sp)\n", register);
+        fileWriter.format("\tsw %s, 0($sp)\n", register);
     }
 
     /* Pops the "top" of the stack and loads it to the given register (e.g. "$v0") */
     public void popToRegisterFromStack(String register) {
-        fileWriter.format("\tlw %s,0($sp)\n", register);
+        fileWriter.format("\tlw %s, 0($sp)\n", register);
         fileWriter.format("\taddu $sp, $sp, 4\n");
     }
 
@@ -198,45 +178,26 @@ public class MIPSGenerator {
     /* ---------------------- Operators and Arithmetic ---------------------- */
 
     public void add(TEMP dst, TEMP oprnd1, TEMP oprnd2) {
-        int i1 = oprnd1.getSerialNumber();
-        int i2 = oprnd2.getSerialNumber();
-        int dstidx = dst.getSerialNumber();
-
-        fileWriter.format("\tadd Temp_%d,Temp_%d,Temp_%d\n", dstidx, i1, i2);
+        fileWriter.format("\tadd %s, %s, %s\n", dst.getRegisterName(), oprnd1.getRegisterName(), oprnd2.getRegisterName());
     }
 
     public void addi(TEMP dst, TEMP temp_oprnd, int int_oprnd) {
         // dst = temp_oprnd + int_oprnd
-        int oprnd_idx = temp_oprnd.getSerialNumber();
-        int dstidx = dst.getSerialNumber();
-
-        fileWriter.format("\taddi Temp_%d, Temp_%d, %d\n", dstidx, oprnd_idx, int_oprnd);
+        fileWriter.format("\taddi %s, %s, %d\n", dst.getRegisterName(), temp_oprnd.getRegisterName(), int_oprnd);
     }
 
     public void sub(TEMP dst, TEMP oprnd1, TEMP oprnd2) {
-        int i1 = oprnd1.getSerialNumber();
-        int i2 = oprnd2.getSerialNumber();
-        int dstidx = dst.getSerialNumber();
-
-        fileWriter.format("\tsub Temp_%d,Temp_%d,Temp_%d\n", dstidx, i1, i2);
+        fileWriter.format("\tsub %s, %s, %s\n", dst.getRegisterName(), oprnd1.getRegisterName(), oprnd2.getRegisterName());
     }
 
     public void mul(TEMP dst, TEMP oprnd1, TEMP oprnd2) {
-        int i1 = oprnd1.getSerialNumber();
-        int i2 = oprnd2.getSerialNumber();
-        int dstidx = dst.getSerialNumber();
-
-        fileWriter.format("\tmul Temp_%d,Temp_%d,Temp_%d\n", dstidx, i1, i2);
+        fileWriter.format("\tmul %s, %s, %s\n", dst.getRegisterName(), oprnd1.getRegisterName(), oprnd2.getRegisterName());
     }
 
     public void div(TEMP dst, TEMP oprnd_left, TEMP oprnd_right) {
         // dst = floor(oprnd_left / oprnd_right)
-        int i_l = oprnd_left.getSerialNumber();
-        int i_r = oprnd_right.getSerialNumber();
-        int dstidx = dst.getSerialNumber();
-
-        fileWriter.format("\tdiv Temp_%d,Temp_%d\n", i_l, i_r);  // quotient stored in special register- lo
-        fileWriter.format("\tmflo Temp_%d\n", dstidx);  // move from lo
+        fileWriter.format("\tdiv %s, %s\n", oprnd_left.getRegisterName(), oprnd_right.getRegisterName());  // quotient stored in special register- lo
+        fileWriter.format("\tmflo %s\n", dst.getRegisterName());  // move from lo
     }
 
 
@@ -254,50 +215,34 @@ public class MIPSGenerator {
     /* ---------------------- Conditional Jump (Branching) ---------------------- */
 
     public void blt(TEMP oprnd1, TEMP oprnd2, String label) {
-        int i1 = oprnd1.getSerialNumber();
-        int i2 = oprnd2.getSerialNumber();
-
-        fileWriter.format("\tblt Temp_%d,Temp_%d,%s\n", i1, i2, label);
+        fileWriter.format("\tblt %s, %s, %s\n", oprnd1.getRegisterName(), oprnd2.getRegisterName(), label);
     }
 
     public void bge(TEMP oprnd1, TEMP oprnd2, String label) {
-        int i1 = oprnd1.getSerialNumber();
-        int i2 = oprnd2.getSerialNumber();
-
-        fileWriter.format("\tbge Temp_%d,Temp_%d,%s\n", i1, i2, label);
+        fileWriter.format("\tbge %s, %s, %s\n", oprnd1.getRegisterName(), oprnd2.getRegisterName(), label);
     }
 
     public void bne(TEMP oprnd1, TEMP oprnd2, String label) {
-        int i1 = oprnd1.getSerialNumber();
-        int i2 = oprnd2.getSerialNumber();
-
-        fileWriter.format("\tbne Temp_%d,Temp_%d,%s\n", i1, i2, label);
+        fileWriter.format("\tbne %s, %s, %s\n", oprnd1.getRegisterName(), oprnd2.getRegisterName(), label);
     }
 
     public void beq(TEMP oprnd1, TEMP oprnd2, String label) {
-        int i1 = oprnd1.getSerialNumber();
-        int i2 = oprnd2.getSerialNumber();
-
-        fileWriter.format("\tbeq Temp_%d,Temp_%d,%s\n", i1, i2, label);
+        fileWriter.format("\tbeq %s, %s, %s\n", oprnd1.getRegisterName(), oprnd2.getRegisterName(), label);
     }
 
     public void beqz(TEMP oprnd1, String label) {
-        int i1 = oprnd1.getSerialNumber();
-
-        fileWriter.format("\tbeq Temp_%d,$zero,%s\n", i1, label);
+        fileWriter.format("\tbeq %s, $zero, %s\n", oprnd1.getRegisterName(), label);
     }
 
     public void bltz(TEMP oprnd1, String label) {
-        int i1 = oprnd1.getSerialNumber();
-        fileWriter.format("\tbltz Temp_%d, %s\n", i1, label);
+        fileWriter.format("\tbltz %s, %s\n", oprnd1.getRegisterName(), label);
     }
 
 
 	/* -------------- (Callee) Functions related MIPS code -------------- */
 	/* Sets to return value ($v0) to be the value in `t` */
 	public void setReturn(TEMP t) {
-		int idx = t.getSerialNumber();
-		fileWriter.format("\tmove $v0,Temp_%d\n", idx);
+		fileWriter.format("\tmove $v0, %s\n", t.getRegisterName());
 	}
 
 	public void functionPrologue(int localsCount) {
@@ -335,7 +280,7 @@ public class MIPSGenerator {
 	}
 
     public void jumpToReturnAddress() {
-        fileWriter.format("\tjr $ra\n");
+        fileWriter.format("\tjr $ra\n\n\n");
     }
 
 
@@ -345,7 +290,7 @@ public class MIPSGenerator {
     public void functionCallerPrologue(List<TEMP> argValues) {
         /* Pushes the arguments to the stack in reverse order */
         for (int i = argValues.size() - 1; i >= 0; i--) {
-            String argValueTemp = String.format("$Temp_%d", argValues.get(i).getSerialNumber());
+            String argValueTemp = String.format("%s", argValues.get(i).getRegisterName());
             pushRegisterToStack(argValueTemp);
         }
     }
@@ -358,10 +303,10 @@ public class MIPSGenerator {
     /* fetches the method-address from vTable and `jalr` to its */
     public void methodJumpAndLink(TEMP classObject, int methodOffset) {
         /* load the vTable pointer to $s0 */
-        fileWriter.format("\tlw $s0 0(TEMP_%d)\n", classObject.getSerialNumber());
+        fileWriter.format("\tlw $s0, 0(TEMP_%d)\n", classObject.getRegisterName());
 
         /* load the method pointer to $s1 */
-        fileWriter.format("\tlw $s1 %d($s0)\n", methodOffset);
+        fileWriter.format("\tlw $s1, %d($s0)\n", methodOffset);
 
         /* jump (and link) to the method */
         fileWriter.format("\tjalr $s1\n");
@@ -373,7 +318,7 @@ public class MIPSGenerator {
 
         if (dstRtn != null) {
             /* Saves the return value to 'dstRtn' */
-            fileWriter.format("\tmove TEMP_%d, $v0\n", dstRtn.getSerialNumber());
+            fileWriter.format("\tmove TEMP_%d, $v0\n", dstRtn.getRegisterName());
         }
     }
 
@@ -381,37 +326,32 @@ public class MIPSGenerator {
     /* ---------------------- Built-in functions ---------------------- */
 
     public void print_int(TEMP t) {
-        int idx = t.getSerialNumber();
-
         /* Prints the integer in 't' */
-        fileWriter.format("\tmove $a0,Temp_%d\n", idx);
-        fileWriter.format("\tli $v0,1\n");
+        fileWriter.format("\tmove $a0, %s\n", t.getRegisterName());
+        fileWriter.format("\tli $v0, 1\n");
         fileWriter.format("\tsyscall\n");
 
         /* Prints space char (as required by exercise's instructions) */
-        fileWriter.format("\tli $a0,32\n");  // space char
-        fileWriter.format("\tli $v0,11\n");
+        fileWriter.format("\tli $a0, 32\n");  // space char
+        fileWriter.format("\tli $v0, 11\n");
         fileWriter.format("\tsyscall\n");
     }
 
     public void print_string(TEMP t) {
-        int idx = t.getSerialNumber();
-
         /* Prints the string in pointed by 't' */
-        fileWriter.format("\tmove $a0,Temp_%d\n", idx);
-        fileWriter.format("\tli $v0,4\n");
+        fileWriter.format("\tmove $a0, %s\n", t.getRegisterName());
+        fileWriter.format("\tli $v0, 4\n");
         fileWriter.format("\tsyscall\n");
     }
 
     /* ---------------------- Other ---------------------- */
 
     public void li(TEMP t, int value) {
-        int idx = t.getSerialNumber();
-        fileWriter.format("\tli Temp_%d,%d\n", idx, value);
+        fileWriter.format("\tli %s, %d\n", t.getRegisterName(), value);
     }
 
     public void move(TEMP dst, TEMP src) {
-        fileWriter.format("\tmove Temp_%d, Temp_%d\n", dst.getSerialNumber(), src.getSerialNumber());
+        fileWriter.format("\tmove %s, %s\n", dst.getRegisterName(), src.getRegisterName());
     }
 
     /* ---------------------- Beginning and Ending of the MIPS code ---------------------- */
@@ -454,7 +394,7 @@ public class MIPSGenerator {
             instance.fileWriter.print("string_invalid_ptr_dref: .asciiz \"Invalid Pointer Dereference\"\n");
 
             /* global labels */
-            instance.fileWriter.format(".text\n");
+            instance.fileWriter.format("\n.text\n");
 
             instance.label(LABEL_STRING_ACCESS_VIOLATION);
             instance.exit_due_to_runtime_check("string_access_violation");
@@ -479,7 +419,7 @@ public class MIPSGenerator {
 
     /* exits the program */
     private void performExit() {
-        fileWriter.print("\tli $v0,10\n");
+        fileWriter.print("\tli $v0, 10\n");
         fileWriter.print("\tsyscall\n");
     }
 
