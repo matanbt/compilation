c7a3295631df394138f703dbbc97d2c12a79b2d9
MatanBT <mtnbt123@gmail.com>
2022-01-28 13:03:36 +0200

    Added Tester MIPS support...
    - Asserts no semantic errors.
    - Asserts valid run of SPIM.
    - Compares mips output to expected output while *ignoring* the annoying prefix (the first lines generated by SPIM).
    - To turn off the logging - set: `flag_save_logs = False`
    - All sorts of output (semantic check, mips generated source, mips run output) are saved in `./output`.

--- ex4/tester.py
+++ ex4/tester.py
@@ -1,3 +1,17 @@
+"""
+    Usage: `$ python3 tester.py` from project's directory.
+
+    First, Make sure:
+        - The relevant compiler is located in `./COMPILER`, the tests located in
+        - The relevant tests are located in `./input` as `txt` files.
+        - Each test has a corresponding `Expected_Output` file in dir `./expected_output`.
+
+    Then, the script outputs:
+        - All the stdout-s of the compilation of the tests in `./logs`.
+        - All the tests outputs (mips sources and semantic checks includes) in `./output`.
+        - If no assertion was raised during the script run, all tested have passed :)
+"""
+
 import subprocess
 import os
 
@@ -8,9 +22,6 @@ expected_output_dir = "./expected_output/"
 logs_dir = "./logs/"
 flag_save_logs = True
 
-test_files = [
-    "TEST_01_Print_Primes.txt"
-     ]
 
 def list_test_files_by_dir():
     global test_files
@@ -20,13 +31,24 @@ def list_test_files_by_dir():
 def run_on_test_files():
     # assumes all files in 'input_files' located in path 'input/'
     for i, filename in enumerate(test_files):
-        input_file =  input_dir + filename                        # input/test.txt
-        output_file = output_dir + filename[:-4] + "_Output.txt"  # output/test_Output.txt
+        input_file =  input_dir + filename                                          # input/test.txt
+        sem_output_file = output_dir + filename[:-4] + "_Semantic_Output.txt"       # output/test_SEMANTIC_OUTPUT.txt
+        mips_source_output_file = output_dir + filename[:-4] + "_MIPS_SOURCE.txt"   # output/test_MIPS_SOURCE.txt
+        mips_run_output_file = output_dir + filename[:-4] + "_MIPS_OUTPUT.txt"      # output/test_MIPS_OUTPUT.txt
+
         print("---- #" + str(i + 1) + " / " + str(len(test_files)) + " | Running: " + filename + " ----")
-        result = subprocess.run(['java', '-jar', 'COMPILER', input_file, output_file],
+        result = subprocess.run(['java', '-jar', 'COMPILER', input_file, sem_output_file],
                                 capture_output=True, text=True)
         assert result.stderr == '', "Got error while running: /n"+ result.stderr
+        with open(sem_output_file) as f:
+            assert f.read().strip() == "OK" , "Got semantic error. L syntax must be obeyed!"
         save_log(filename, result.stdout)
+        os.rename(src=output_dir + "MIPS.txt", dst=mips_source_output_file)
+
+        with open(mips_run_output_file, 'w') as f:
+            result = subprocess.run(['spim', '-f', mips_source_output_file],
+                                    text=True, stdout=f)
+            assert result.returncode == 0, "Got Error while compiling the generated mips \n" + result.stderr
 
 
 def simple_tester():
@@ -36,11 +58,13 @@ def simple_tester():
     for i, filename in enumerate(test_files):
         print("---- #" + str(i + 1) + " / " + str(len(test_files)) + " | Checking: " + filename + " ----")
         input_file =  input_dir + filename                                             # input/test.txt
-        output_file = output_dir + filename[:-4] + "_Output.txt"                       # output/test_Output.txt
+        output_file = output_dir + filename[:-4] + "_MIPS_OUTPUT.txt"                  # output/test_MIPS_OUTPUT.txt
         expected_file = expected_output_dir + filename[:-4] + "_Expected_Output.txt"   # expected_output/test_Expected_Output.txt
         with open(output_file, 'r') as f1, open(expected_file, 'r') as f2:
-            s1 = f1.read().strip()
-            s2 = f2.read().strip()
+            s1 = f1.read().strip().split('\n',maxsplit=5)[5:]  # split & slicing will remove mips' annoying prefix
+            s2 = f2.read().strip().split('\n',maxsplit=5)[5:]
+            s1, s2 = ''.join(s1), ''.join(s2)
+
             assert s1 == s2, "\n---> Test failed on: " + filename +\
                              "\n---> Got: " + s1 + "   Expected: " + s2 +\
                              "\n---> Please check : " + logs_dir + filename[:-4] + "_log_run.txt" +\
