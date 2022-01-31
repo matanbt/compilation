1cc084e71bd2816d116bff9f8f019266350fc9d5
Omer Koskas <omerk96@gmail.com>
2022-01-31 16:36:16 +0200

    Added Omer's new tests

--- ex4/test_generator.py
+++ ex4/test_generator.py
@@ -6,6 +6,7 @@ TEST_GENERATOR_FILE_PATHS_LIST = [
     "./tests/tests.txt",
     "./tests/matan_tests_1.txt",
     "./tests/matan_tests_2.txt",
+    "./tests/omer_tests_v2.txt"
 ]
 TEST_INPUT_DIR = "./input"
 TEST_OUTPUT_DIR = "./expected_output"
